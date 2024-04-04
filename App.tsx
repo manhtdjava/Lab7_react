fa49473a04677f81ebd3ffe1528712b7402fcee1
manhtdjava <141570639+manhtdjava@users.noreply.github.com>
2024-04-05 01:13:10 +0700

    first commit

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import App71 from './insertFB'
+
+const App = () => {
+  return (
+    <App71 />
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
